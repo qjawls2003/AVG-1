a225eb9bc766e56b82fc7ed00762e8e14a5baade
hsteele <thatwouldbehaley@gmail.com>
2018-09-06 10:48:08 -0400

    Newest centering code

--- servo_centering/servo_centering.ino
+++ servo_centering/servo_centering.ino
@@ -23,13 +23,13 @@ void setup()
   rightServo.attach(10);   // attaches the servo on pin 10 to the servo object 
   leftServo.attach(11);
   //This is one of the servo ports on the Parallax shield. Others are 11, 12, and 13
-  /*
+  
   for (i = 0; i < dur; i++ ){ 
     rightServo.writeMicroseconds(1500);    // motor stop 
     leftServo.writeMicroseconds(1500);    // motor stop 
     delay(15); 
   }
-
+  
   for (i = 0; i< dur; i++){
      rightServo.writeMicroseconds(1000);     // full speed in one direction
      leftServo.writeMicroseconds(1000);
@@ -46,7 +46,7 @@ void setup()
   rightServo.writeMicroseconds(1500);    // motor stop 
   leftServo.writeMicroseconds(1500);
   delay(15); 
-  }*/
+  }
 
   
 // test that code did what it was supposed to do (omit or replace with your algorithm)
@@ -63,13 +63,13 @@ void setup()
      delay(15);  
   }*/
   
-  turnRight();
-  motorStop();
-  turnLeft();
+  forward(120);
+  motorStop(20);
+  turnRight(20);
+  motorStop(20);
+  turnLeft(20);
   
-  
-   rightServo.writeMicroseconds(1500);    // motor stop 
-   leftServo.writeMicroseconds(1500);
+  motorStop(1);
   
 } 
  
@@ -78,26 +78,34 @@ void loop()
 
 } 
 
-void turnRight(){
-  for (i = 0; i< dur*29; i++){
+void turnRight(int len){
+  for (i = 0; i< dur*len; i++){
      rightServo.writeMicroseconds(1250);     // Clockwise 45 degrees 
      leftServo.writeMicroseconds(1750);
      delay(15);  
   }
 }
 
-void turnLeft(){
-  for (i = 0; i< dur*11; i++){
+void turnLeft(int len){
+  for (i = 0; i< dur*len; i++){
      rightServo.writeMicroseconds(1750);     // counterclockwise 45 degrees
      leftServo.writeMicroseconds(1250);
      delay(15);  
   }
 }
 
-void motorStop(){
-  for (i = 0; i < dur*29; i++ ){ 
+void motorStop(int len){
+  for (i = 0; i < dur*len; i++ ){ 
     rightServo.writeMicroseconds(1500);    // motor stop 
     leftServo.writeMicroseconds(1500);
+  delay(15); 
+  }
+}
+  
+void forward(int len){
+  for (i = 0; i < dur*len; i++ ){ 
+    rightServo.writeMicroseconds(2000);    // motor stop 
+    leftServo.writeMicroseconds(2000);
   delay(15); 
   }
 }
